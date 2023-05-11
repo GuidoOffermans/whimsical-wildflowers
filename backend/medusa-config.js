fb01734fb48f5e5f5a9e8e148dc62b4514aba6b9
Guido Offermans <guidooffermans@hotmail.com>
2023-05-11 11:26:41 +0200

    delete console.log

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -22,7 +22,7 @@ try {
     dotenv.config({path: path.join(__dirname, "..", ENV_FILE_NAME)});
 } catch (e) {
 }
-console.log(process.env)
+
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
     process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
